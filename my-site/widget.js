b4d5a0c3d400bc71be6c100534918cae106c2268
Roy Prins <prinsroy@gmail.com>
2018-12-13 22:00:16 +0100

    iframe resize

--- my-site/widget.js
+++ my-site/widget.js
@@ -2,14 +2,19 @@
 let braincards = (function(window, undefined){
   
   let braincards = {};
+  let iframe = document.createElement('iframe');
+  iframe.style.width = '100%';
+  iframe.style.height= "0";  // Set a low initial height, as resizer doesn't handle it properly
 
-  function loadScript(url, callback, integrity=undefined, crossOrigin='anonymous') {
+  function loadScript(url, integrity=undefined, callback=undefined) {
     const script = document.createElement('script');
     script.async = true;
     script.src = url;
     script.onload = function() { callback() };
-    if(integrity) script.integrity = integrity;
-    script.crossOrigin = crossOrigin;
+    if(integrity) {
+      script.integrity = integrity;
+      script.crossOrigin = 'anonymous';
+    }
     // TODO optimize: query once outside fuction
     const entry = document.getElementsByTagName('script')[0];
     entry.parentNode.insertBefore(script, entry);
@@ -19,43 +24,53 @@ let braincards = (function(window, undefined){
    * Locate the script tag that loads this script
    * return the element, and all params (including site-name)
    */
-  function getEntryElementAndShortName(){
+  function getElementAndParams(){
+    const params = braincard_config  // TODO security: trusting the user input here
     const scripts = document.body.getElementsByTagName('script');
+    const siteRegex = /http:\/\/127\.0\.0\.1:8080\/([a-zA-Z0-9-]+)\/widget\.js/;
     let element, src, matches;
-    const siteRegex = /http:\/\/127\.0\.0\.1:8080\/([a-zA-Z0-9-]+)\/widget\.js/
 
     for (element of scripts) {
       src = element.src;
       if (src) {
         matches = src.match(siteRegex)
         if (matches) {
-          return [matches[1], element];
+          params.shortname = matches[1]
+          return [element, params];
         }
       }
     }
     return null;
   }
 
+
   function loadSupportingFiles(callback) {
-    console.log("testing2")
     loadScript(
-      'https://code.jquery.com/jquery-3.3.1.min.js', 
-      function(){ $( "h1" ).css( "border", "9px solid red" ); },
-      'sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=')
+      'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.6.3/iframeResizer.min.js', 
+      'sha256-DyeqmZcGhOAc1ZUAyHN3cS9xC3HsFj27zcWVbK6/m0I=',
+      function(){ 
+        console.log("Converting iframe into resizable one....")
+        iframe = iFrameResize({log:true}, iframe) 
+      },
+      )
   }
 
-  function getWidgetParams() {}
-  function getCardData(params, callback) {}
-  function drawWidget() {}
 
-  loadSupportingFiles(function() {
-    const params  = getWidgetParams();
-    getCardData(params, function() {
-      drawWidget();
-    });
-  });
+  loadSupportingFiles();
 
-  getEntryElementAndShortName()
+  const [target, params] = getElementAndParams();
+
+
+  // TODO security: trusting the user with number of params and value (no escaping)
+  const queryParams = Object.entries(params).map(([k, v]) => `${k}=${v}`).join('&')
+  const url = 'http://127.0.0.1:8080/embed.html'+'?'+queryParams
+  console.log(url);
+  iframe.src = url
+ 
+  iframe.style.border = 'none';
+
+  target.parentNode.insertBefore(iframe, target);
+
 
   return braincards;
 })();
